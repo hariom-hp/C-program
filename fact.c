7842aa2c3269a181f932f55091ca760dbc36a563
Hariom paswan <115986980+hariom-hp@users.noreply.github.com>
2024-01-03 22:57:56 +0530

    find the factorial given nuber
     Implicit function

---
+++ fact.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int length,width,area;
+    printf("enter the any two nuber ");
+    scanf("%d%d",&length,&width);
+    area=length*width;
+    printf("rectangle is  is :%d",area);
+    return 0;
+}
